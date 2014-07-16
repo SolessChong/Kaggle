05f1b174481b102a16528252c3b161f6b3ec48b0
Solesschong <solesschong@gmail.com>
2014-07-16 16:02:18 +0800

    Add output variable NN and SAE into predict_SAE. Tune predict_SAE parameters.

--- Digi_Recognizer/src/main.m
+++ Digi_Recognizer/src/main.m
@@ -5,24 +5,24 @@ Yexp = expandY(Y);
 nTrial = 12;
 error_array = zeros(nTrial,1);
 
-parfor k = 1 : nTrial
+for k = 1 : nTrial
     CVO = cvpartition(Y,'holdout',1/3);
     err = zeros(CVO.NumTestSets,1);
     for i = 1 : CVO.NumTestSets
         trInd = CVO.training(i);
         teInd = CVO.test(i);
 
-        [labels, nn] = predict_SAE(X(trInd,:), Yexp(trInd,:), X(teInd,:));
+        [labels, nn{k}, sae{k}] = predict_SAE(X(trInd,:), Yexp(trInd,:), X(teInd,:));
 
         err(i,1) = sum(Y(teInd) ~= labels);
     end
     
     error_array(k) = 1 - sum(err)/CVO.TestSize;
     
-    disp(error_array(k));
 end
 
 disp(error_array);
+disp(mean(error_array));
 
 %labels = predict_SAE(X, Yexp, test);
 
