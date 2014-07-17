aedc8540c3141c75c66aa0806ce50e857bf08d82
Solesschong <solesschong@gmail.com>
2014-07-17 13:50:41 +0800

    Add CIFAR dataset

---
+++ Digi_Recognizer/src/classifier/predict_CNN_CIFAR.m
@@ -0,0 +1,43 @@
+function [labels, cnn] = predict_CNN_CIFAR(X, Y, test)
+%
+% Predict via CNN
+%
+% Train and predict
+%
+global config
+
+train_x = double(reshape(X', 32, 32, size(X,1))) / 255;
+test_x = double(reshape(test', 32, 32, size(test,1))) / 255;
+train_y = double(Y');
+
+%% Train a 6c-2s-12c-2s Convolutional neural network 
+%will run 1 epoch in about 200 second and get around 11% error. 
+%With 100 epochs you'll get around 1.2% error
+cnn.layers = {
+    struct('type', 'i')
+    struct('type', 'c', 'outputmaps', 6, 'kernelsize', 5); % conv. layer
+    struct('type', 's', 'scale', 2) % sub sampling layer
+    struct('type', 'c', 'outputmaps', 12, 'kernelsize', 5)
+    struct('type', 's', 'scale', 2)
+    };
+cnn = cnnsetup(cnn, train_x, train_y);
+
+opts.alpha = 1;
+opts.batchsize = 50;
+opts.numepochs = 200;
+cnn = cnntrain(cnn, train_x, train_y, opts);
+
+labels = cnnpredict(cnn, test_x)' - 1;
+
+% plot MSE
+figure; plot(cnn.rL);
+
+end
+
+
+function [labels, net] = cnnpredict(cnn, X)
+
+net = cnnff(cnn, X);
+[~, labels] = max(net.o);
+
+end
---
+++ Digi_Recognizer/src/main_CIFAR.m
@@ -0,0 +1,30 @@
+% Load data
+%[X, Y, test] = loadData();
+Yexp = expandY(labels);
+
+nTrial = 12;
+error_array = zeros(nTrial,1);
+
+parfor k = 1 : nTrial
+    CVO = cvpartition(Y,'holdout',1/3);
+    err = zeros(CVO.NumTestSets,1);
+    for i = 1 : CVO.NumTestSets
+        trInd = CVO.training(i);
+        teInd = CVO.test(i);
+
+        %[labels, nn{k}, sae{k}] = predict_SAE(X(trInd,:), Yexp(trInd,:), X(teInd,:));
+        [labels, cnn{k}] = predict_CNN(X(trInd,:), Yexp(trInd,:), X(teInd,:));
+
+        err(i,1) = sum(Y(teInd) ~= labels);
+    end
+    
+    error_array(k) = 1 - sum(err)/CVO.TestSize;
+    
+end
+
+disp(error_array);
+disp(mean(error_array));
+
+%labels = predict_SAE(X, Yexp, test);
+
+%genSubmission(labels, 'SAE');
